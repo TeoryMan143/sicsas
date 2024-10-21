7aca365f85863f0ca5607dda6f0fdcee1010e4bb
TeoryMan143 <jonathancortestm143@gmail.com>
2024-10-21 09:45:32 -0500

    feat: vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,18 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
+
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     react(),
   ],
+
+  adapter: vercel(),
 });
