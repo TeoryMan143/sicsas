eb9af43a423327011d344694e3861d8af679d6d7
TeoryMan143 <jonathancortestm143@gmail.com>
2024-10-22 08:54:33 -0500

    refactor: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
-
 import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
