81911bc02dbc587e06009adeaaf6bbd32b68a1c9
TeoryMan143 <jonathancortestm143@gmail.com>
2024-10-27 12:18:09 -0500

    feat: desktop index

--- src/components/Header/contact-info.tsx
+++ src/components/Header/contact-info.tsx
@@ -9,7 +9,7 @@ function ContactInfo() {
     lg:flex'
     >
       {CONTACT_DATA.map(({ icon: Icon, content, copy }) => (
-        <Copiable addToClipboard={copy}>
+        <Copiable addToClipboard={copy} key={copy}>
           <li className='flex gap-3 items-center justify-cente cursor-pointer'>
             <Icon />
             <span className='font-questrial text-sm'>{content}</span>
--- src/components/index-carousel.tsx
+++ src/components/index-carousel.tsx
@@ -31,42 +31,53 @@ function IndexCarousel() {
   return (
     <section className='bg-carousel bg-cover relative py-8'>
       <div className='absolute top-0 left-0 size-full bg-slate-950/60 z-10' />
-      <div className='relative z-20 space-y-5'>
-        <SecTitle className='line-clamp-2'>{TITLES[current]}</SecTitle>
-        <Carousel opts={{ loop: true }} setApi={setApi}>
-          <CarouselContent>
+      <div
+        className='
+          relative z-20 space-y-5 
+          lg:grid lg:grid-cols-2 lg:px-8
+        '
+      >
+        <div className='lg:grid lg:place-content-center'>
+          <SecTitle className='line-clamp-2 lg:line-clamp-4 lg:text-4xl lg:leading-[3.5rem] lg:text-left lg:pr-12 lg:ml-16 '>
+            {TITLES[current]}
+          </SecTitle>
+        </div>
+        <div>
+          <Carousel opts={{ loop: true }} setApi={setApi}>
+            <CarouselContent className='lg:px-20'>
+              {Array.from({ length: 3 }).map((_, i) => (
+                <CarouselItem className='lg:basis-full' key={i}>
+                  <img
+                    className='object-cover aspect-video'
+                    src={`img/car${i + 1}.webp`}
+                    alt='Sistemas contra incendios'
+                  />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious className='left-2 opacity-75' />
+            <CarouselNext className='right-2 opacity-75' />
+          </Carousel>
+          <div className='flex gap-2 justify-center relative z-20 mt-5'>
             {Array.from({ length: 3 }).map((_, i) => (
-              <CarouselItem key={i}>
-                <img
-                  className='object-cover aspect-video '
-                  src={`img/car${i + 1}.webp`}
-                  alt='Sistemas contra incendios'
+              <button
+                onClick={() => {
+                  api?.scrollTo(i);
+                }}
+                className='p-1'
+                key={i}
+              >
+                <div
+                  className={cn(
+                    'h-1.5 w-14 rounded-lg bg-white/45 transition-colors',
+                    {
+                      'bg-white/95': current == i,
+                    },
+                  )}
                 />
-              </CarouselItem>
+              </button>
             ))}
-          </CarouselContent>
-          <CarouselPrevious className='left-2 opacity-75' />
-          <CarouselNext className='right-2 opacity-75' />
-        </Carousel>
-        <div className='flex gap-2 justify-center'>
-          {Array.from({ length: 3 }).map((_, i) => (
-            <button
-              onClick={() => {
-                api?.scrollTo(i);
-              }}
-              className='p-1'
-              key={i}
-            >
-              <div
-                className={cn(
-                  'h-1.5 w-14 rounded-lg bg-white/45 transition-colors',
-                  {
-                    'bg-white/95': current == i,
-                  },
-                )}
-              />
-            </button>
-          ))}
+          </div>
         </div>
       </div>
     </section>
