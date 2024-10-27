7c8af13cf3c0837867b47455c2b807950cb5903f
TeoryMan143 <jonathancortestm143@gmail.com>
2024-10-27 10:06:28 -0500

    feat: products arrow

--- src/components/products/menu.tsx
+++ src/components/products/menu.tsx
@@ -38,7 +38,23 @@ function ProductsMenu() {
               active:scale-95
             '
           >
-            <ChevronsUpDown className='h-4 w-4 opacity-50 absolute top-1/2 -translate-y-1/2 left-3' />
+            <svg
+              className={cn(
+                'text-xl absolute top-1/2 -translate-y-1/2 left-3 transition-transform',
+                {
+                  '-rotate-90': open,
+                },
+              )}
+              xmlns='http://www.w3.org/2000/svg'
+              width='1em'
+              height='1em'
+              viewBox='0 0 24 24'
+            >
+              <path
+                fill='currentColor'
+                d='M6.325 12.85q-.225-.15-.337-.375T5.874 12t.113-.475t.337-.375l8.15-5.175q.125-.075.263-.112T15 5.825q.4 0 .7.288t.3.712v10.35q0 .425-.3.713t-.7.287q-.125 0-.262-.038t-.263-.112z'
+              />
+            </svg>
             <span>
               {productTypes.find(prod => prod.value === value)?.label}
             </span>
